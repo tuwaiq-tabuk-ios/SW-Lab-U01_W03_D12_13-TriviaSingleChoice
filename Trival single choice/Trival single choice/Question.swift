0f346f372164fb07d9011c648b191f683257e06e
Ameera BA <alqraniameera@outlook.com>
2021-10-20 11:54:11 +0300

    complete Trivia Single Choice

---
+++ Trival single choice/Trival single choice/Question.swift
@@ -0,0 +1,16 @@
+//
+//  Question.swift
+//  Trival single choice
+//
+//  Created by Ameera BA on 19/10/2021.
+//
+
+import Foundation
+
+
+struct Question {
+  
+  let TheQuestions : String!
+  let Answer : String!
+ // let Answer : Int!
+}
